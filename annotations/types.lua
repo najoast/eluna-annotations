6f65b83dad5aebc30576b35791f4b560aed6d5d0
zhouyan <zhouyan@xd.com>
2022-12-01 12:29:37 +0800

    hello, world

---
+++ annotations/types.lua
@@ -0,0 +1,20 @@
+
+---@alias int number
+---@alias uint number
+---@alias uint8 number
+---@alias uint16 number
+---@alias uint32 number
+---@alias uint64 number
+---@alias int8 number
+---@alias int16 number
+---@alias int32 number
+---@alias int64 number
+
+---@alias float number
+---@alias float32 number
+---@alias float64 number
+---@alias double number
+
+---@alias bool boolean
+
+---@alias ObjectGuid number
